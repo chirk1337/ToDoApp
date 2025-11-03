444d932a7cab6499d06c5d724e72b615ed00a24f
chirk1337 <chiribas74@gmail.com>
2025-11-04 02:37:24 +0500

    парсинг JSON

--- ToDoApp/Services/DataService.swift
+++ ToDoApp/Services/DataService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+enum DataError: Error {
+    case fileNotFound
+    case decodingError(Error)
+}
+protocol DataServiceProtocol {
+    func loadInitialTasks(completion: @escaping ( Result<[ToDoDTO],DataError> )-> Void)
+}
+
+final class DataService: DataServiceProtocol {
+    func loadInitialTasks(completion: @escaping (Result<[ToDoDTO], DataError>) -> Void) {
+        guard let url = Bundle.main.url(forResource: "todos", withExtension: "json") else {
+            completion(.failure(.fileNotFound))
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let response = try JSONDecoder().decode(TodDoResponse.self, from: data)
+            completion(.success(response.todos))
+        } catch {
+            completion(.failure(.decodingError(error)))
+        }
+    }
+}
