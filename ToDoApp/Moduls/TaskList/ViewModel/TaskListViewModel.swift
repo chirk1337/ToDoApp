a17e2068d81d2eb12e08e2c5a174206829b7d2af
chirk1337 <chiribas74@gmail.com>
2025-11-04 18:04:17 +0500

    работа с ViewModel

--- ToDoApp/Moduls/TaskList/ViewModel/TaskListViewModel.swift
+++ ToDoApp/Moduls/TaskList/ViewModel/TaskListViewModel.swift
@@ -43,32 +43,127 @@ final class TaskListViewModel: NSObject,  TaskListViewModelProtocol {
     }
     
     func getNumberOfRows() -> Int {
-        <#code#>
+        return fetchedResultController.sections?.first?.numberOfObjects ?? 0
+
     }
     
     func getTask(at index: IndexPath) -> Task {
-        <#code#>
+        return fetchedResultController.object(at: index)
     }
     
     func viewDidLoad() {
-        <#code#>
+        setupFetchedResultController()
+        checkAndLoadInitialData()
     }
     
     func addNewTaskTapped() {
-        <#code#>
+        coordonator.showAddTaskAlert { [weak self] title in
+            self?.createTask(title: title)
+        }
     }
     
     func deleteTask(at index: IndexPath) {
-        <#code#>
+        let task = getTask(at: index)
+        coreDataStack.viewContext.delete(task)
+        coreDataStack.saveContext()
     }
     
     func toggleTaskStatus(at index: IndexPath) {
-        <#code#>
+        let task = getTask(at: index)
+        task.isCompleted.toggle()
+        coreDataStack.saveContext()
     }
     
     func searchTasks(witn query: String) {
-        <#code#>
+        let predicate: NSPredicate?
+        if query.isEmpty {
+            predicate = nil
+        } else {
+            predicate = NSPredicate(format: "title CONTAINS[cd] %@", query)
+        }
+        fetchedResultController.fetchRequest.predicate = predicate
+        
+        do {
+            try fetchedResultController.performFetch()
+            onDataUpdated?()
+        } catch {
+            onError?("Error during search: \(error.localizedDescription)")
+        }
     }
+    
+    
+    private func checkAndLoadInitialData() {
+        let key = "isInitialDataLoaded"
+        let defaults = UserDefaults.standard
+        if !defaults.bool(forKey: key) {
+            
+            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+                self?.dataService.loadInitialTasks { result in
+                    switch result {
+                    case .success(let dtos):
+                        self?.saveInitialTasksToCoreData(dtos: dtos)
+                        defaults.set(true, forKey: key)
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            self?.onError?("Failed to load initial data: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func saveInitialTasksToCoreData(dtos: [ToDoDTO]) {
+        coreDataStack.performBackground { context in
+            dtos.forEach { dto in
+            let task = Task(context: context)
+                task.id = Int64(dto.id)
+                task.title = dto.todo
+                task.isCompleted = dto.completed
+                task.creationDate = Date()
+                task.taskDescription = ""
+            }
+            
+            do {
+                try context.save()
+            } catch {
+                print("Failed to save initial data: \(error)")
+            }
+        }
+    }
+    
+    private func createTask(title: String) {
+        let context = coreDataStack.viewContext
+        let newTask = Task(context: context)
+        newTask.title = title
+        newTask.creationDate = Date()
+        newTask.isCompleted = false
+        newTask.id = -1
+        newTask.taskDescription = ""
+        
+        coreDataStack.saveContext()
+    }
+    
+    private func setupFetchedResultController() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        let sortDescriptor1 = NSSortDescriptor(key: "isCompleted", ascending: true)
+        let sortDescriptor2 = NSSortDescriptor(key: "creationDate", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor1, sortDescriptor2]
+        
+        fetchedResultController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                             managedObjectContext: coreDataStack.viewContext,
+                                                             sectionNameKeyPath: nil,
+                                                             cacheName: nil)
+        fetchedResultController.delegate = self
+        
+        do {
+            try fetchedResultController.performFetch()
+        } catch {
+            onError?("Failed to fetch tasks: \(error.localizedDescription)")
+        }
+    }
+    
  
 }
 extension TaskListViewModel: NSFetchedResultsControllerDelegate {
